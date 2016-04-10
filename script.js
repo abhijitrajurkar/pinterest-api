3093c3b688879eeb326cc1fb7b07ba6a04976244
Iulia <iulitaro@gmail.com>
2016-04-10 23:13:18 +0200

    Added HTML body

--- script.js
+++ script.js
@@ -1,2 +1,63 @@
+var Const = { ONE_WEEK: 1000 * 60 * 60 * 24 * 7,    
+    PIN_APP: '4826889351902276742',
+    PIN_FIELDS: 'id,name,image[small]',
+    PIN_SCOPE: 'read_public, write_public'
+            }
 
+var Pinterest = {
+    /*
+     *  Use the SDK to login to Pinterest
+     *  @param {Function} callback - function fired on completion
+     */
+    login: function(callback) {
+        PDK.login({ scope : Const.PIN_SCOPE }, callback);
+    },
+    /*
+     *  Use the SDK to logout of Pinterest
+     */
+    logout: function() {
+        PDK.logout();
+    },
+    /*
+     *  Use DK to determine auth state of user
+     *  @returns {Boolean}
+     */
+    loggedIn: function() {
+        return !!PDK.getSession();
+    },
+    /*
+     *  Use SDK to create a new Pin
+     *  @param {Object}   data     - {board, note, link, image_url}
+     *  @param {Function} callback - function fired on completion
+     */
+    createPin: function(data, callback) {
+        PDK.request('/pins/', 'POST', data, callback);
+    },
+    /*
+     *  Use SDK to request current users boards
+     *  @param {Function} callback - function fired on completion
+     */
+    myBoards: function(callback) {
+        PDK.me('boards', { fields: Const.PIN_FIELDS }, callback);
+    }
+};
+
+// one time init of the application
+PDK.init({ appId: Const.PIN_APP, cookie: true });
+
+function   resetState() {
+        var state = {            
+            pinterest: Pinterest.loggedIn()
+        };
+    console.log("in reset state");
+        return state;
+    }
+
+var resetState = resetState();
+
+function  pinLogin() {
+        Pinterest.login(resetState);
+    };
+
+pinLogin();
 
